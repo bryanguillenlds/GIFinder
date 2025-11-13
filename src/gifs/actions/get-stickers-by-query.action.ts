d621a50fe49fa4c1236efa7c6ba8786093906f10
Bryan Guillén <bryanguillenlds@gmail.com>
2025-11-13 10:09:23 -0400

    Testing Error on bad request and empty query check
    - Added a check to return an empty array if the query is empty.
    - Implemented error handling to catch API errors and log them to the console.
    - Updated tests to cover the new behavior for empty queries and error scenarios.

--- src/gifs/actions/get-stickers-by-query.action.ts
+++ src/gifs/actions/get-stickers-by-query.action.ts
@@ -3,20 +3,30 @@ import type { Sticker } from "../interfaces/sticker.interface";
 import { giphyApi } from "../api/giphy.api";
 
 export const getStickersByQuery = async (query: string): Promise<Sticker[]> => {
-  const { data: response } = await giphyApi.get<GiphyResponse>(`/search`, {
-    params: {
-      q: query,
-      limit: 10,
-      offset: 0,
-      lang: "en",
-    },
-  });
+  if (!query.trim()) {
+    return [];
+  }
 
-  return response.data.map((sticker) => ({
-    id: sticker.id,
-    title: sticker.title,
-    url: sticker.images.original.url,
-    width: Number(sticker.images.original.width),
-    height: Number(sticker.images.original.height),
-  }));
+  try {
+    const { data: response } = await giphyApi.get<GiphyResponse>(`/search`, {
+      params: {
+        q: query,
+        limit: 10,
+        offset: 0,
+        lang: "en",
+      },
+    });
+
+    return response.data.map((sticker) => ({
+      id: sticker.id,
+      title: sticker.title,
+      url: sticker.images.original.url,
+      width: Number(sticker.images.original.width),
+      height: Number(sticker.images.original.height),
+    }));
+  } catch (error) {
+    console.error(error);
+
+    return [];
+  }
 };
